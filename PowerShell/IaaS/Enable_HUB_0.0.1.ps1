d202a930358d6cec58da20a8915341c2b9e72838
Abramov <theabramoff@yandex.ru>
2023-05-22 16:14:45 +0800

    Portfolio update #3

--- PowerShell/IaaS/Enable_HUB_0.0.1.ps1
+++ PowerShell/IaaS/Enable_HUB_0.0.1.ps1
@@ -17,7 +17,7 @@ foreach ($Subscription in $Subscriptions)
 {
 (Select-AzSubscription $Subscription).Subscription.Name
 
-Get-AzVm -Status | Where-Object {$_.StorageProfile.OsDisk.OsType -eq "Windows" -and $_.LicenseType -ne "Windows_Server" -and $_.Powerstate -eq "VM running" } | Select-Object -Property Name, @{Label="VmSize";Expression={$_.HardwareProfile.VmSize}}, @{Label="OsType";Expression={$_.StorageProfile.OsDisk.OsType}}, @{Label="PowerState";Expression={$_.PowerState}} | ft >> $file
+Get-AzVm -Status | Where-Object {$_.StorageProfile.OsDisk.OsType -eq "Windows" -and $_.LicenseType -ne "Windows_Server" -and $_.Powerstate -eq "VM running" } | Select-Object -Property Name, @{Label="VmSize";Expression={$_.HardwareProfile.VmSize}}, @{Label="OsType";Expression={$_.StorageProfile.OsDisk.OsType}}, @{Label="PowerState";Expression={$_.PowerState}} | Format-Table >> $file
 
 foreach ($vm in $VMsWithoutAHB)
 
